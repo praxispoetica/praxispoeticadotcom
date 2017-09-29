178df93dadd025a9d24606eb32be0f2e42da5bfa
victorkane <victorkane@gmail.com>
2017-09-29 11:12:41 -0400

    Author name used from populated textAuthor field and i18n date filter applied for individual text rendering for #11

--- pages/textos/_id.vue
+++ pages/textos/_id.vue
@@ -3,11 +3,11 @@
     <v-layout row wrap>
       <v-flex xs12>
         <v-card class="ma-1">
-          <v-card-title class="headline mb-1">{{text.metaData.itemSlug}}</v-card-title>
+          <v-card-title class="headline mb-1">{{ text.metaData.itemSlug }}</v-card-title>
           <v-card-text>
-            <p class="primary--text subheading mt-0">{{text.metaData.itemSlug}}</p>
+            <p class="primary--text subheading mt-0">{{ text.textAuthor[0].metaData.itemName }}</p>
             <blockquote class="body-1" v-html="text.textBody.value"></blockquote>
-            <p class="primary--text mt-2">{{text.metaData.publishedDate}}</p>
+            <p class="primary--text mt-2">{{ text.metaData.publishedDate | dateEsLa }}</p>
           </v-card-text>
           <v-card-actions><p><nuxt-link to="/textos">Volver a la lista de texts</nuxt-link></p></v-card-actions>
         </v-card>
